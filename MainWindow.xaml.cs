182120f49eb71d3552a4896ec3155b12193424c0
vikrampg <vikram.pg@metamation.com>
2024-04-05 18:26:36 +0530

    Update - Can do basic scribbles with mouse button

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,20 +1,30 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ScribblePad;
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
+
 public partial class MainWindow : Window {
    public MainWindow () {
       InitializeComponent ();
+   }
+   private Point mCurrentPoint = new ();
+
+   private void ScribblePad_MouseDown (object sender, MouseButtonEventArgs e) {
+      if (e.ButtonState == MouseButtonState.Pressed) mCurrentPoint = e.GetPosition (this);
+   }
+
+   private void ScribblePad_MouseMove (object sender, MouseEventArgs e) {
+      if (e.LeftButton == MouseButtonState.Pressed) {
+         Line line = new () {
+            Stroke = System.Windows.Media.Brushes.White,
+            X1 = mCurrentPoint.X,
+            Y1 = mCurrentPoint.Y,
+            X2 = e.GetPosition (this).X,
+            Y2 = e.GetPosition (this).Y,
+         };
+         mCurrentPoint = e.GetPosition (this);
+         scribblePad.Children.Add (line);
+      }
    }
 }
